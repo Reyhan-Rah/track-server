9b7614e73c2596b304379a41dfcc935a443af6b3
Reyhan <r.rahmanipour@gmail.com>
2020-05-09 19:33:03 +0430

    hashing,salting,signin added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
+require('./models/User');
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
+const requireAuth = require('./middlewares/requireAuth');
 
 const app = express();
 
-const mongoUri= 'mongodb+srv://admin:passwordpassword@cluster0-bheye.mongodb.net/anything?retryWrites=true&w=majority'
+app.use(bodyParser.json());
+app.use(authRoutes);
+
+const mongoUri= 'mongodb+srv://admin:passwordpassword@cluster0-bheye.mongodb.net/test?retryWrites=true&w=majority'
 mongoose.connect(mongoUri, {
     useNewUrlParser: true,
     useCreateIndex: true,
@@ -18,8 +25,8 @@ mongoose.connection.on('error', (err) => {
     console.error('Error connecting to mongo', err)
 });
 
-app.get('/', (req,res) => {
-    res.send('Hi there!');
+app.get('/', requireAuth, (req,res) => {
+    res.send(`Your Email: ${req.user.email}`);
 });
 
 app.listen (3000, () => {
